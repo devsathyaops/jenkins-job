c6d5c7c6c26dda3ee81a29ac23538c731e720f5f
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-07-12 16:31:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ resource "aws_spot_instance_request" "spot_req" {
 
   tags = {
     Name = "spot-ins"
+    value = "${var.server_job}.spot${count.index + 1}"
   }
   provisioner "local-exec" {
       command = "aws ec2 create-tags --resources ${self.spot_instance_id} --tags Key=Name,Value=${var.server_job}-spot-instances-${count.index} --region ${var.aws_region}"
