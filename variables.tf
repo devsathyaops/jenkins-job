55f5a1ab168fe340ec84fb857fd6e4ff1a795ede
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-07-12 14:56:49 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,23 +11,27 @@ type = string
 }
 
 variable "spot_price" {
+type = string
 }
 
 variable "server_job" {
+type = string
 }
 
 variable "instance_count" {
+type = string
 }
 
 variable "type"{
+type = string
 }
 
 variable "subnet_id" {
-
+type = string
 }
 
 variable "security_group" {
-    
+    type = string
 }
 
 variable "iam_role" {
