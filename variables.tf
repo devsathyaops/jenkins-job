20e01be5d8c359778dfb5ec5dcadadbccd2e31d3
devsathyaops <98995934+devsathyaops@users.noreply.github.com>
2022-07-12 15:31:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,38 +2,3 @@ variable "aws_region" {
     default = "us-west-2"
 }
 
-variable "source_instance_id"{
- type = string
-}
-
-variable "instance_type" {
-type = string
-}
-
-variable "spot_price" {
-type = string
-}
-
-variable "server_job" {
-type = string
-}
-
-variable "instance_count" {
-type = string
-}
-
-variable "type"{
-type = string
-}
-
-variable "subnet_id" {
-type = string
-}
-
-variable "security_group" {
-    type = string
-}
-
-variable "iam_role" {
-    
-}
